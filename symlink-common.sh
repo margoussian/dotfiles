7802ad2a77e18c713af59b119de21179eeeee74a
Vaheh Margoussian <vmargoussian@edmunds.com>
2023-05-26 14:48:40 -0700

    add tmux config

--- symlink-common.sh
+++ symlink-common.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# git
 ln -sf $(pwd)/git/.gitignore $HOME/.gitignore
 ln -sf $(pwd)/git/.gitconfig $HOME/.gitconfig
+
+# tmux
+ln -sf $(pwd)/prefs/tmux.conf $HOME/.tmux.conf
 
 # mkdir -p $HOME/.config/fish
 # ln -sf $(pwd)/fish/functions "$HOME/.config/fish"
